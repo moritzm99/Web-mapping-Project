9b246a764e6207ec75392491d95c3907558ae466
moritzm99 <moritzfr@ut.ee>
2024-05-17 17:23:56 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -7,21 +7,3 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'OpenStreetMap contributors'
 });
 osm.addTo(map);
-
-addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
-// add geoJSON layer
-async function addGeoJson(url) {
-const response = await fetch(url)
-const data = await response.json()
-const heatData = data.features.map(heatDataConvert)
-const heatMap = L.heatLayer(heatData, { radius: 10 })
-heatMap.addTo(map)
-}
-
-function heatDataConvert(feature) {
-return [
-feature.geometry.coordinates[1],
-feature.geometry.coordinates[0],
-feature.properties.area,
-]
-}
