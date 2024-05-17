19332060fd59be99c4f87d20deebd236e8cc7640
moritzm99 <moritzfr@ut.ee>
2024-05-17 16:25:34 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -19,10 +19,6 @@ async function addCelltowersGeoJson(url) {
 }
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson');
 
-// Function to reset the map to default settings
-function defaultMapSettings() {
-  map.setView([58.373523, 26.716045], 12);
-}
 // default map settings
 function defaultMapSettings() { map.setView([58.373523, 26.716045], 12)
 }
