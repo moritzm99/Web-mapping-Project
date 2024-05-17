7e71fb6f1da804837fb2c32479c23ab67b4be7c8
moritzm99 <moritzfr@ut.ee>
2024-05-17 12:17:43 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,8 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
-const osm = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}', {
-maxZoom: 19,
-attribution: 'OpenStreetMap contributors', })
+var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+});
 osm.addTo(map)
-
-
