76a206c74875060f316082907f2d6c8f65bde82d
moritzm99 <moritzfr@ut.ee>
2024-05-17 16:24:04 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -8,50 +8,6 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 });
 osm.addTo(map);
 
-// Function to determine color based on feature property
-function getColor(property) {
-  switch (property) {
-    case 1:
-      return '#ff0000';
-    case 13:
-      return '#009933';
-    case 6:
-      return '#0000ff';
-    case 7:
-      return '#ff0066';
-    default:
-      return '#ffffff';
-  }
-}
-
-// Define the style for polygons
-function polygonStyle(feature) {
-  return {
-    fillColor: getColor(feature.properties.OBJECTID),
-    fillOpacity: 0.5,
-    weight: 1,
-    opacity: 1,
-    color: 'grey'
-  };
-}
-
-// Function to bind popup information to each feature
-function popUPinfo(feature, layer) {
-  layer.bindPopup(feature.properties.NIMI);
-}
-
-// Asynchronously load and add GeoJSON polygons layer to the map
-async function addDistrictsGeoJson(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-  const polygons = L.geoJson(data, {
-    onEachFeature: popUPinfo,
-    style: polygonStyle
-  });
-  polygons.addTo(map);
-}
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson');
-
 // Asynchronously load and add GeoJSON layer for cell towers, using clustering
 async function addCelltowersGeoJson(url) {
   const response = await fetch(url);
